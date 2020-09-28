05cdeea4d8365009ab629c40623bdbbdeab9905e
grabitel1605 <grabitel1605@gmail.com>
2020-09-27 22:02:16 -0500

    Added to python folder

---
+++ Python/user_input.py
@@ -0,0 +1,7 @@
+name = input("Hello!, What is your name?:  ")
+print("Ah, Hello "+ name + "!")
+
+number = input("What is your favorite number?: ")
+print("OK, So your favorite number is " + number + "!")
+triple = int(number) * 3
+print("I like the number " + str(triple) + "!")
---
+++ Python/variable_types.py
@@ -0,0 +1,10 @@
+my_integer = 3
+my_float = 3.141592
+my_string = "Hello"
+
+print(type(my_float))
+print(type(my_integer))
+print(type(my_string))
+
+my_integer = "WTF"
+print(type(my_integer))
